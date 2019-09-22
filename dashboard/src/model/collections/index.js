3eb9cafbb7b7f3b285fd0fa0069ff75d41b09aec
Gerard Lamusse <>
2019-09-22 11:02:56 +0200

    Added Todo and inital graphql code

--- dashboard/src/model/collections/index.js
+++ dashboard/src/model/collections/index.js
@@ -4,10 +4,10 @@ import posts from './posts'
 import tags from './tags'
 import topics from './topics'
 import users from './users'
-import attributes from './attributes';
-import categories from './categories';
-import products from './products';
-import reviews from './reviews';
+import attributes from './attributes'
+import categories from './categories'
+import products from './products'
+import reviews from './reviews'
 
 export default [
   attributes,
---
+++ graphql/create-gql-schema.js
@@ -0,0 +1,7 @@
+const { gql } = require('apollo-server');
+const cTypes = require( '../dashboard/src/model/collections' )
+
+
+cTypes.forEach((cType, cIndex) => {
+
+})
