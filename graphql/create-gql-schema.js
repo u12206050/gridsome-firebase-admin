580c72d21ebcdcb20bfcdbafa87c915934d9f753
Gerard Lamusse <>
2019-09-25 12:07:49 +0200

    Added .env.example

--- graphql/create-gql-schema.js
+++ graphql/create-gql-schema.js
@@ -1,6 +1,7 @@
 const { gql } = require('apollo-server');
 const cTypes = require( '../dashboard/src/model/collections' )
 
+// Possible solution: https://graphql-compose.github.io/docs/plugins/plugin-json.html
 
 cTypes.forEach((cType, cIndex) => {
 
